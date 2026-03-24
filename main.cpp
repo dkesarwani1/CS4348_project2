3bc2cd687c10d77ccaff126dfee7125b46143c89
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-03-23 22:00:12 -0500

    Added Teller relation

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ using namespace std;
 counting_semaphore<2> door(2); // door allows 2 customers at a time
 counting_semaphore<1> manager(1);//1 manager available
 counting_semaphore<2> safe(2);// safe allows 2 customers at a time
-counting_semaphore<> customerready();
+counting_semaphore<50> customerready(0); // semaphore to signal when a customer is ready to be served
+mutex servedlock; // mutex to protect access to the served customers count
+int servedCustomers = 0; // count of customers served
 queue <int> customerQueue; // queue to hold waiting customers
 mutex queueMutex; // mutex to protect access to the customer queue
 void customer(int id)
@@ -24,13 +26,64 @@ void customer(int id)
     // Simulate service time
     this_thread::sleep_for(chrono::milliseconds(rand() % 2000));
     cout << "Customer " << id << " has been served." << endl;
+
 }
 void teller(int id)
 {    
     while (true) {
         // Simulate teller availability
+        cout<< "Teller " << id << " is ready to serve." << endl;
+        // Wait for a customer to be ready
+        customerready.acquire();
+        // Get the next customer from the queue
+        int customerId;
+        {
+            lock_guard<mutex> lock(queueMutex);
+            if (!customerQueue.empty()) {
+                customerId = customerQueue.front();
+                customerQueue.pop();
+            } else {
+                continue; // No customers in the queue, continue waiting
+            }
+        }
+        cout << "Teller " << id << " is serving customer " << customerId << "." << endl;
+            // Simulate service time
+        this_thread::sleep_for(chrono::milliseconds(rand() % 2000));
+        cout << "Teller " << id << " has finished serving customer " << customerId << "." << endl;
+
+        string transactiontype = (rand() % 2 == 0) ? "Deposit" : "Withdrawal";
+        cout << "Customer " << customerId << " is performing a " << transactiontype << "." << endl;
+        // Simulate transaction time
+        this_thread::sleep_for(chrono::milliseconds(rand() % 2000));
+        if(transactiontype == "Withdrawal")
+        {
+           cout<<"Teller " << id << " Serving Customer "<<customerId<<" is waiting for Manager."<<endl;
+              manager.acquire();
+            cout<<"Manager is now serving Teller " << id << " with Customer "<<customerId<<" for withdrawal."<<endl;
+            this_thread::sleep_for(chrono::milliseconds(rand() % 26 + 5));
+            cout<<"Manager has finished serving Teller " << id << " with Customer "<<customerId<<" for withdrawal."<<endl;
+            manager.release();
+        }
+        cout<<"Teller "<<id<< "Going to safe with Customer "<<customerId<<" for "<<transactiontype<<"."<<endl;
+        safe.acquire();
+        cout<<"Teller "<<id<< " is at the safe with Customer "<<customerId<<" for "<<transactiontype<<"."<<endl;
+        this_thread::sleep_for(chrono::milliseconds(rand()%41 + 10)); // Simulate time at the safe
+        cout<<"Teller "<<id<< " has finished at the safe with Customer "<<customerId<<" for "<<transactiontype<<"."<<endl;
+        safe.release();
+        cout <<"Teller " << id << " has completed the transaction for customer " << customerId << "." << endl;
+        
+        {
+            lock_guard<mutex> lock(servedlock);
+            servedCustomers++;
+            if(servedCustomers == 50) 
+            {
+                cout << "All customers have been served. Teller " << id << " is closing." << endl;
+                break; // Exit the loop if all customers have been served
+            }
+        }
         
     }
+
 }
 int main() 
 {
